31519a3c78fa1cc0e90b02cbb82aaa8e4d74b72b
Danilo de Souza <danilodesousa@gmail.com>
2020-11-10 14:54:06 -0300

    Test oc rollout

---
+++ java-hello/src/main/java/br/com/minishift/java/minishiftjava/ClientController.java
@@ -0,0 +1,15 @@
+package br.com.minishift.java.minishiftjava;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ClientController {
+
+	@GetMapping("hello")
+	public String hello() {
+		return "Hello Client";
+	}
+}
