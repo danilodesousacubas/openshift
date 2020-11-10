aa85977f1af9eefce4cea54afd716875975a4a70
Danilo de Souza <danilodesousa@gmail.com>
2020-11-10 13:59:52 -0300

    Sample spring-boot project

---
+++ java-hello/src/main/java/br/com/minishift/java/minishiftjava/MinishiftJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.minishift.java.minishiftjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinishiftJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinishiftJavaApplication.class, args);
+	}
+
+}
---
+++ java-hello/src/test/java/br/com/minishift/java/minishiftjava/MinishiftJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.minishift.java.minishiftjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinishiftJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
