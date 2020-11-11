0fa2350387e88ca8dfef8fdcc050e9e1c957c985
Danilo de Souza <danilodesousa@gmail.com>
2020-11-11 10:02:25 -0300

    Test openshift / oc rolling-rollout

--- java-hello/src/main/java/br/com/minishift/java/minishiftjava/ClientController.java
+++ java-hello/src/main/java/br/com/minishift/java/minishiftjava/ClientController.java
@@ -13,7 +13,7 @@ public class ClientController {
 
 	@GetMapping("hello")
 	public String hello() {
-		log.info("Hello Client!");
+		log.info("Hello Client!! Rolling!!");
 		return "Hello Client";
 	}
 }
