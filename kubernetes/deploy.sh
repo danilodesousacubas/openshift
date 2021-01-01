5f2ba64fb9e48bd4e1b549ce49aedf990b2c8ff4
Danilo de Souza <danilodesousa@gmail.com>
2021-01-01 14:03:10 -0300

    update deployments

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 kubectl apply -f purchase-deployment.yaml
-kubectl apply -f purchase-service.yaml
+kubectl apply -f clients-deployments.yaml
+
+#kubectl apply -f products-deployments.yaml
+
